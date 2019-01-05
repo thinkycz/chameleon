00bc46aa88b7fe666cd813e7aea6aaceabe48b8f
leo <leo@nulisec.com>
2019-01-05 11:32:01 +0100

    Laravel mix tailwind updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
+let mix = require('laravel-mix');
+require('laravel-mix-tailwind');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,10 +14,7 @@ const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.js')],
-    });
+    .tailwind();
 
 if (mix.inProduction()) {
     mix.version();
